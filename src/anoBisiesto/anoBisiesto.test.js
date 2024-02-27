6b865df4b8943e83d9653f1865169e3e610a96ae
Patrick <patrickgonzalesquintana@gmail.com>
2024-02-27 00:08:33 -0400

    Año no bisiesto (divisible por 100 pero no por 400) por ejemplo 1900

--- src/anoBisiesto/anoBisiesto.test.js
+++ src/anoBisiesto/anoBisiesto.test.js
@@ -16,4 +16,8 @@ describe("Sumar", () => {
     it("deberia darnos que es bisiesto cuando el año es divisible por 400 por ejemplo el 400", () => {
         expect(esBisiesto(400)).toEqual(true);
     });
+
+    it("Año no bisiesto (divisible por 100 pero no por 400) por ejemplo 1900", () => {
+        expect(esBisiesto(1900)).toEqual(false);
+    });
 });
