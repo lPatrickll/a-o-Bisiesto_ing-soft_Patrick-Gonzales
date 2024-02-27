379de41db189a9183aeae47032015cadb4d6731b
Patrick <patrickgonzalesquintana@gmail.com>
2024-02-26 23:44:30 -0400

    Se arreglo un error en el test

--- src/anoBisiesto/anoBisiesto.test.js
+++ src/anoBisiesto/anoBisiesto.test.js
@@ -2,6 +2,8 @@ import esBisiesto from "./anoBisiesto";
 
 describe("Sumar", () => {
     it("deberia darnos que es bisiesto por ejemplo el 2024", () => {
-        expect(esBisiesto(2025)).toEqual(true);
+        expect(esBisiesto(2024)).toEqual(true);
     });
+
+    
 });
